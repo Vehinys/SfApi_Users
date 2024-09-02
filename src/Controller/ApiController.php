104d8da732cfb897519adc7d91c8e340815aaf6d
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-09-02 11:33:34 +0200

    MODIFICATION DU CONTROLLER API

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\HttpClient\ApiHttpClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ApiController extends AbstractController
 {
-    #[Route('/api', name: 'app_api')]
-    public function index(): Response
-    {
+    #[Route('/users', name: 'users_list')]
+    public function index(
+        
+        ApiHttpClient $apiHttpClient
+        
+    ): Response {
+
+        $users = $apiHttpClient->getUsers();
         return $this->render('api/index.html.twig', [
-            'controller_name' => 'ApiController',
+            'users' => '$users',
         ]);
     }
 }
